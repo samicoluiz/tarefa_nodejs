b0abab551a66ba5b2be6db90f08c62a64b4a0c30
samico <loouiz@gmail.com>
2023-03-31 13:13:14 -0300

    api 1.0

--- infectados2m.js
+++ infectados2m.js
@@ -1,13 +1,27 @@
 const express = require("express");
+const fetch = (...args) => import('node-fetch')
+  .then(({ default: fetch }) => fetch(...args));
 
-const app = express()
+const app = express();
+const PORT = 4001;
+const url = 'https://api.apify.com/v2/key-value-stores/TyToNta7jGKkpszMZ/records/LATEST?disableRedirect=true';
 
-const PORT = process.env.PORT || 4001;
 
-app.get("/infectados2m", (req, res, next) => {
-    console.log(req)
-  });
+app.get('/api/infectados2m', async (req, res) => {
+  try {
+    const apiResponse = await fetch(url);
+    const apiResponseJson = await apiResponse.json();
+    const relevantData = apiResponseJson.infectedByRegion;
+    const infectados2m = relevantData.filter(relevantData => relevantData.count > 2000000);
+
+    console.log(apiResponseJson);
+    res.json(infectados2m);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send('Algo deu errado');
+  }
+});
 
 app.listen(PORT, () => {
-    console.log(`O servidor está escutando na porta ${PORT}`);
-  });
+  console.log(`O servidor está escutando na porta ${PORT}`);
+});
